62c6b29ccb4eb1d2bb268029ca0f1f45a5b30670
lylee010-debug <lylee010@gmail.com>
2026-03-19 23:39:37 +0800

    更新

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'my-website-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// 安装：缓存核心文件
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// 请求：优先用缓存，没有再联网获取
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then(response => {
+            if (!response || response.status !== 200) {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => cache.put(event.request, responseToCache));
+            return response;
+          });
+      })
+  );
+});
+
+// 更新：清理旧缓存
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+});
